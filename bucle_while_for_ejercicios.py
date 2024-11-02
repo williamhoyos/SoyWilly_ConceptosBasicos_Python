43210f4c7f1cf929d0b54d7ad1f82c9aaa2dd14c
williamhoyos <williamja.hoyos@gmail.com>
2024-11-02 14:10:23 -0300

    bucles V01

---
+++ bucle_while_for_ejercicios.py
@@ -0,0 +1,27 @@
+"""
+Bucles while
+El bucle while se ejecuta mientras una condición sea True. Se utiliza cuando no se sabe exactamente cuántas veces se debe repetir el bloque de código.
+
+contador = 1
+while contador <= 5:
+    print("Esta es la vuelta número", contador)
+    contador += 1
+--------------------------------------------------------------------------------------------------
+Bucles for
+El bucle for se usa para iterar sobre una secuencia, como una lista, cadena o rango de números. Es útil cuando sabes cuántas veces quieres que se ejecute el bloque de código.
+
+for numero in range(1, 6):
+    print("Esta es la vuelta número", numero)
+--------------------------------------------------------------------------------------------------
+"""
+
+"""
+Ejercicio 1: Contador Ascendente Crea un bucle while que cuente de 1 a 10 e imprima cada número. Luego, realiza el mismo ejercicio usando un bucle for.
+"""
+#Escribe aqui tu codigo:
+
+"""
+Ejercicio 2: Suma de Números Pide al usuario que ingrese un número positivo (por ejemplo, 5) y usa un bucle for para calcular la suma de todos los números 
+            desde 1 hasta el número ingresado. Imprime el resultado.
+"""
+#Escribe aqui tu codigo:
