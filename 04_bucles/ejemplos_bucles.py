57a116d6bb834f88248dad710257aee31a1e58d5
williamhoyos <williamja.hoyos@gmail.com>
2024-11-02 15:25:46 -0300

    mejora ejercicios

--- 04_bucles/ejemplos_bucles.py
+++ 04_bucles/ejemplos_bucles.py
@@ -16,12 +16,19 @@ for numero in range(1, 6):
 """
 
 """
-Ejercicio 1: Contador Ascendente Crea un bucle while que cuente de 1 a 10 e imprima cada número. Luego, realiza el mismo ejercicio usando un bucle for.
+Ejercicio 1: 
+Crea un programa que elija un número aleatorio entre 1 y 10 (puedes usar random.randint(1, 10) para generar este número). Luego, pide al usuario que adivine el número, y usa 
+un bucle while para darle otra oportunidad hasta que acierte. Al acertar, felicítalo e imprime el número de intentos que le tomó adivinar.
+
+Pistas:
+Necesitarás importar la librería random.
+Usa un contador para llevar el conteo de intentos.
 """
 #Escribe aqui tu codigo:
 
 """
-Ejercicio 2: Suma de Números Pide al usuario que ingrese un número positivo (por ejemplo, 5) y usa un bucle for para calcular la suma de todos los números 
-            desde 1 hasta el número ingresado. Imprime el resultado.
+Ejercicio 2: 
+Solicita al usuario que ingrese un número y luego utiliza un bucle for para imprimir la tabla de multiplicar de ese número, desde el 1 hasta el 10.
+Usa el bucle for para iterar del 1 al 10 y realiza la multiplicación en cada paso.
 """
 #Escribe aqui tu codigo:
